bc6154313adaccce9434887cb4bea47534936ef1
Shubh <shubh230405@gmail.com>
2025-12-17 15:46:47 +0530

    Changes in backend

--- server/index.js
+++ server/index.js
@@ -15,10 +15,7 @@ app.use(express.urlencoded({ extended: true }));
 // MongoDB Connection
 const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/rabuste-coffee';
 
-mongoose.connect(MONGODB_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
+mongoose.connect(MONGODB_URI)
 .then(() => console.log('MongoDB Connected'))
 .catch(err => console.error('MongoDB Connection Error:', err));
 
